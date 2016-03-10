87b67bd0f40aa81c1dff076e48d330fbd11f2349
vladolalik <wladolalik@gmail.com>
2016-03-10 19:35:00 +0100

    added experiment scenario

--- logger.php
+++ logger.php
@@ -1,13 +1,6 @@
 <?php
-    echo $_REQUEST["param"];
-//
-//    $file = 'people.txt';
-//    // Open the file to get existing content
-//    $current = file_get_contents($file);
-//    // Append a new person to the file
-//    $current .= $_REQUEST["param"];
-//    // Write the contents back to the file
-//    file_put_contents($file, $current);
-    file_put_contents('logs.txt', $_REQUEST["param"].PHP_EOL , FILE_APPEND);
-
+    $data = file_get_contents('php://input');
+    $filename = $_COOKIE["username"]."_logs.txt";
+    file_put_contents($filename, $data.PHP_EOL , FILE_APPEND);
+    var_dump($filename);
 ?>
