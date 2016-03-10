611448d54747b8a0670d9908eb0a78427f4367bb
vladolalik <wladolalik@gmail.com>
2016-03-10 02:14:00 +0100

    initial

--- logger.php
+++ logger.php
@@ -1,1 +1,13 @@
-<?php echo $_POST['param']; ?>
+<?php
+    echo $_REQUEST["param"];
+//
+//    $file = 'people.txt';
+//    // Open the file to get existing content
+//    $current = file_get_contents($file);
+//    // Append a new person to the file
+//    $current .= $_REQUEST["param"];
+//    // Write the contents back to the file
+//    file_put_contents($file, $current);
+    file_put_contents('logs.txt', $_REQUEST["param"].PHP_EOL , FILE_APPEND);
+
+?>
